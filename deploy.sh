f5c86777e93d891991c93ffdb3142db8d3d81858
arsavag <arsavag94@gmail.com>
2021-07-28 20:52:25 +0400

    deploy

--- deploy.sh
+++ deploy.sh
@@ -17,7 +17,7 @@ git add -A
 git commit -m 'deploy'
 
 # if you are deploying to https://<USERNAME>.github.io
-# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
+# git push -f git@github.com:arsavag/arsavag.github.io.git master
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
 git push -f git@github.com:arsavag/chat-app.git master:gh-pages
